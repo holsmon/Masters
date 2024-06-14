ed8ae1c8da7a322ef64732259d451ebf5e58b51f
Hollie Mullin <80362101+holsmon@users.noreply.github.com>
2024-06-14 17:43:37 -0400

    Add files via upload

---
+++ Custom_atlases/transform_bna_power_to_MNI152.py
@@ -0,0 +1,6 @@
+#In terminal, I typed the following to resample bna and power into MNI 152
+
+
+3dresample -master /storage/group/nad12/default/fgh3/Users/Hollie/xcp_d/custom_atlases/templates/mni152_versions/space-MNI152NLin6Asym_atlas.nii.gz -prefix bna_MNI152.nii.gz -input /storage/group/nad12/default/fgh3/Users/Hollie/xcp_d/custom_atlases/templates/original/BN_Atlas_274_MNI.nii.gz
+
+3dresample -master /storage/group/nad12/default/fgh3/Users/Hollie/xcp_d/custom_atlases/templates/mni152_versions/space-MNI152NLin6Asym_atlas.nii.gz -prefix power_MNI152.nii.gz -input /storage/group/nad12/default/fgh3/Users/Hollie/xcp_d/custom_atlases/templates/original/power264MNI.nii.gz
